5d2c1ff3c275dcceec89af02de15418003155d62
rkaune <rkaune@7HPL6Y1.Student.UGDSB.ED>
2018-11-27 15:05:40 -0500

    AnimalsTester from U6A2

---
+++ src/animalstester/Dog.java
@@ -0,0 +1,12 @@
+package animalstester;
+
+public class Dog extends Mammal {
+    public void speak() {
+        System.out.println("BARK!!!");
+    }
+
+    //Unique method for Dog Class
+    public void wagTail() {
+        System.out.println("Tail Wagging...");
+    }
+}
